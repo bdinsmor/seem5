122054ec89b8bcfc0f76f43dbb18577e3d32daaf
Brian Dinsmore <bdins@Brians-iMac.fios-router.home>
2018-12-23 13:41:42 -0500

    update

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,4 +1,3 @@
-import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
@@ -8,13 +7,7 @@ import { MapComponent } from './map.component';
 
 @NgModule({
   declarations: [MapComponent],
-  imports: [
-    CommonModule,
-    MapRoutingModule,
-    LeafletModule.forRoot(),
-    AgmCoreModule.forRoot({ apiKey: "AIzaSyA6j5cBJN-pIetAHWVBttgypZqOdUSllQ4" })
-  ],
-  providers: [GoogleMapsAPIWrapper],
+  imports: [CommonModule, MapRoutingModule, LeafletModule.forRoot()],
   exports: [MapComponent],
   entryComponents: [MapComponent]
 })

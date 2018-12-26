6b5b45e329746987803068ae82c8b4c7d37bd959
Brian Dinsmore <bdinsmor@gmail.com>
2018-12-26 09:02:14 -0500

    fullcalendar

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,16 +1,81 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild
+} from "@angular/core";
+import { CalendarComponent } from "ng-fullcalendar";
+import { Options } from "fullcalendar";
+import { EventsService } from "../shared/services/events.service";
 @Component({
-  selector: 'app-events',
-  templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss'],
+  selector: "app-events",
+  templateUrl: "./events.component.html",
+  styleUrls: ["./events.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EventsComponent implements OnInit {
+  events = [];
+  displayEvent: any;
+  calendarOptions: Options;
+  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
 
-  constructor() { }
+  constructor(private eventService: EventsService) {}
 
   ngOnInit() {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+      this.calendarOptions = {
+        editable: true,
+        eventLimit: false,
+        header: {
+          left: "prev,next today",
+          center: "title",
+          right: "month,agendaWeek,agendaDay,listMonth"
+        },
+        selectable: true,
+        events: this.events
+      };
+    });
+  }
+  clearEvents() {
+    this.events = [];
+  }
+  loadEvents() {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+    });
   }
 
+  clickButton(model: any) {
+    this.displayEvent = model;
+  }
+  eventClick(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title,
+        allDay: model.event.allDay
+        // other params
+      },
+      duration: {}
+    };
+    this.displayEvent = model;
+  }
+  updateEvent(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title
+        // other params
+      },
+      duration: {
+        _data: model.duration._data
+      }
+    };
+    this.displayEvent = model;
+  }
 }
--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { EventsRoutingModule } from './events-routing.module';
-import { EventsComponent } from './events.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FullCalendarModule } from "ng-fullcalendar";
+import { EventsRoutingModule } from "./events-routing.module";
+import { EventsComponent } from "./events.component";
 
 @NgModule({
   declarations: [EventsComponent],
-  imports: [
-    CommonModule,
-    EventsRoutingModule
-  ],
+  imports: [CommonModule, FullCalendarModule, EventsRoutingModule],
   exports: [EventsComponent],
   entryComponents: [EventsComponent]
 })
-export class EventsModule { }
+export class EventsModule {}

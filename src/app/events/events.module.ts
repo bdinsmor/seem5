cbaa581636a00b4d203d6c1290de06af999baf9d
Brian Dinsmore <bdinsmor@gmail.com>
2018-12-26 09:44:39 -0500

    sidenav for event details

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from "@angular/common";
 import { FullCalendarModule } from "ng-fullcalendar";
 import { EventsRoutingModule } from "./events-routing.module";
 import { EventsComponent } from "./events.component";
+import { MatSidenavModule, MatButtonModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [EventsComponent],
-  imports: [CommonModule, FullCalendarModule, EventsRoutingModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    FullCalendarModule,
+    EventsRoutingModule
+  ],
   exports: [EventsComponent],
   entryComponents: [EventsComponent]
 })
